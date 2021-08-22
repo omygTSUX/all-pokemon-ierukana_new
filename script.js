e374cab4b1a197f7db7f2e700d40143d624c98e4
jmiyamoto <souvenirtsux@yahoo.co.jp>
2021-08-22 14:50:23 +0900

    回答フォーム要素を追加、CSV読み込み関数を追加

--- script.js
+++ script.js
@@ -40,9 +40,43 @@ function createTable(number_pokemons) {
     }
 }
 
+// 全ポケモンリストを読み込む関数
+function getCSV(){
+    var req = new XMLHttpRequest(); // HTTPでファイルを読み込むためのXMLHttpRrequestオブジェクトを生成
+    req.open("get", "all_pokemon.csv", true); // アクセスするファイルを指定
+    req.send(null); // HTTPリクエストの発行
+	
+    // レスポンスが返ってきたらconvertCSVtoArray()を呼ぶ	
+    req.onload = function(){
+	convertCSVtoArray(req.responseText); // 渡されるのは読み込んだCSVデータ
+    }
+}
+
+// 読み込んだCSVデータを二次元配列に変換する関数convertCSVtoArray()の定義
+function convertCSVtoArray(str){ // 読み込んだCSVデータが文字列として渡される
+    var result = []; // 最終的な二次元配列を入れるための配列
+    var tmp = str.split("\n"); // 改行を区切り文字として行を要素とした配列を生成
+ 
+    // 各行ごとにカンマで区切った文字列を要素とした二次元配列を生成
+    for(var i=0;i<tmp.length;++i){
+        result[i] = tmp[i].split(',');
+    }
+ 
+    alert(result[1][1]);
+}
+
 // ページロード時に自動実行する関数
 window.onload = function () {
     var number_pokemons = 900;
     setNumberOfPokemons(number_pokemons);
     createTable(number_pokemons);
+    getCSV();
 }
+
+// 回答ボタンを押した時に実行される関数
+document.getElementById("form_answer").onsubmit = function() {
+    var value = document.getElementById("input_answer").value;
+    var td = document.getElementById('pokemon_1');
+    td.innerHTML = value;
+    return false;
+  };
