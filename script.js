d83329506fd09f033382e6cd7f5aabbb6ab8ab88
jmiyamoto <souvenirtsux@yahoo.co.jp>
2021-08-22 12:07:15 +0900

    Create script.js, Write createTable()

---
+++ script.js
@@ -0,0 +1,23 @@
+var number_pokemons = 900;
+
+var createTable = (function (number_pokemons){
+    // ヘッダー行を追加する
+    var thead = document.getElementsByClassName('thead_pokedex_number');
+    var tr = document.createElement('tr');
+    for (var c = 0; c < Math.ceil(number_pokemons / 100); c++) {
+        var th = document.createElement('th');
+        th.innerHTML = (c*100 + 1) + "〜" + ((c+1)*100);
+    }
+    thead.appendChild(tr)
+
+    // ボディを追加する
+    var tbody = document.getElementsByClassName('tbody_pokemons');
+    for (var r = 0; r < 100; r++) {
+        var tr = document.createElement('tr');
+        for (var c = 0; c < Math.ceil(number_pokemons / 100); c++) {
+          var td = document.createElement('td');
+          tr.appendChild(td);
+        }
+        tbody.appendChild(tr);
+    }
+})(number_pokemons);
