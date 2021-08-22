b398449e6e0ddbcd7a9a73dd99e96acc1d6ec8ff
jmiyamoto <souvenirtsux@yahoo.co.jp>
2021-08-22 12:48:23 +0900

    ページロード時にJSが自動実行するように変更５

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
-function createTable (number_pokemons) {
+
+// 表を動的に生成する関数
+function createTable(number_pokemons) {
     // ヘッダー行を追加する
     var thead = document.getElementById('thead_pokedex_number');
     var tr = document.createElement('tr');
     for (var c = 0; c < Math.ceil(number_pokemons / 100); c++) {
         var th = document.createElement('th');
-        th.innerHTML = (c*100 + 1) + "〜" + ((c+1)*100);
+        th.innerHTML = (c * 100 + 1) + "〜" + ((c + 1) * 100);
+        tr.appendChild(th);
     }
     thead.appendChild(tr);
 
@@ -13,13 +16,14 @@ function createTable (number_pokemons) {
     for (var r = 0; r < 100; r++) {
         var tr = document.createElement('tr');
         for (var c = 0; c < Math.ceil(number_pokemons / 100); c++) {
-          var td = document.createElement('td');
-          tr.appendChild(td);
+            var td = document.createElement('td');
+            tr.appendChild(td);
         }
         tbody.appendChild(tr);
     }
 }
 
+// ページロード時に自動実行する関数
 window.onload = function () {
     var number_pokemons = 900;
     createTable(number_pokemons);
